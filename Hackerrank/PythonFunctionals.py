c1c6f548464557f5d0f1f802f3ccd66498cfcb16
| | /≥ \/ |_ <113752537+Wondamonstaa@users.noreply.github.com>
2024-01-23 15:13:17 -0600

    Update PythonFunctionals.py

--- Hackerrank/PythonFunctionals.py
+++ Hackerrank/PythonFunctionals.py
@@ -34,3 +34,19 @@ if __name__ == '__main__':
 filtered_emails = filter_mail(emails)
 filtered_emails.sort()
 print(filtered_emails)
+
+
+#Reduce
+from fractions import Fraction
+from functools import reduce
+
+def product(fracs):
+    t = reduce(lambda x, y: x*y, fracs)
+    return t.numerator, t.denominator
+
+if __name__ == '__main__':
+    fracs = []
+    for _ in range(int(input())):
+        fracs.append(Fraction(*map(int, input().split())))
+    result = product(fracs)
+    print(*result)
