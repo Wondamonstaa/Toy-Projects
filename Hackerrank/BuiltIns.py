759a9951ae9f8fd57dd675a71f34dd12479aa0fd
| | /≥ \/ |_ <113752537+Wondamonstaa@users.noreply.github.com>
2024-01-23 15:54:58 -0600

    Update BuiltIns.py

--- Hackerrank/BuiltIns.py
+++ Hackerrank/BuiltIns.py
@@ -5,3 +5,26 @@ n = int(input_str[0])
 x = int(input_str[1])
 
 [print(sum(i)/len(i)) for i in list(zip(*[list(map(float, input().split())) for _ in range(x)]))]
+
+
+#String composition
+s = input()
+
+low, up, odd, even = [], [], [], []
+
+for char in s:
+    if char.islower():
+        low.append(char)
+        low.sort()
+    elif char.isupper():
+        up.append(char)
+        up.sort()
+    elif char.isdigit() and int(char) % 2 != 0:
+        odd.append(char)
+        odd.sort()
+    elif char.isdigit() and int(char) % 2 == 0:
+        even.append(char)
+        even.sort()
+
+result = ''.join(low + up + odd + even)
+print(result)
