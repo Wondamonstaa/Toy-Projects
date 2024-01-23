897978f207e17640038a08739ea54b89d0ed6068
| | /≥ \/ |_ <113752537+Wondamonstaa@users.noreply.github.com>
2024-01-22 23:40:30 -0600

    Update Sets.py

--- Hackerrank/Sets.py
+++ Hackerrank/Sets.py
@@ -63,3 +63,20 @@ try:
     
 except ValueError:
     print("Incorrect bounds!")
+
+
+#DIFFERENCE
+try:
+
+    a = int(input())
+    if a < 1000:
+        s1 = set(map(int, input().split(' ')))
+    
+    b = int(input())
+    if b < 1000:
+        s2 = set(map(int, input().split(' ')))
+    
+    print(len(s1.difference(s2)))
+    
+except ValueError:
+    print("Incorrect bounds!")
