8d1452497c05ca18c6ee0579fc86b7029619d88e
| | /≥ \/ |_ <113752537+Wondamonstaa@users.noreply.github.com>
2024-01-23 16:17:26 -0600

    Update Itertools.py

--- Hackerrank/Itertools.py
+++ Hackerrank/Itertools.py
@@ -6,3 +6,16 @@ b = list(map(int, input().split()))
 
 #Unpacks the list of tuples using *
 print(*list(product(a, b)))
+
+#Itertools.permutations()
+from itertools import permutations
+
+string, size = input().split()
+
+# Get the sorted permutations
+sorted_perms = sorted(permutations(string, int(size)))
+
+# Print each permutation on a separate line
+for perm in sorted_perms:
+    print(''.join(perm))
+
