ca08222f5b5286dae57d0877d563ea1fff0723e7
| | /≥ \/ |_ <113752537+Wondamonstaa@users.noreply.github.com>
2024-01-22 21:56:27 -0600

    Create Sets.py

---
+++ Hackerrank/Sets.py
@@ -0,0 +1,11 @@
+n, m = (int(i) for i in input().split(' '))
+
+S = [int(i) for i in input().split(' ')]
+
+A = set([int(i) for i in input().split(' ')])
+B = set([int(i) for i in input().split(' ')])
+
+#C = set([int(i) for i in input().split(' ')]) list comprehension
+#D = set([int(i) for i in input().split(' ')])
+
+print(sum((1 for i in S if i in A)) - sum((1 for i in S if i in B)))
