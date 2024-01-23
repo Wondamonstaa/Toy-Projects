b29c93263538846729f7fe18118472a271e880bb
| | /≥ \/ |_ <113752537+Wondamonstaa@users.noreply.github.com>
2024-01-22 23:18:45 -0600

    Update Sets.py

--- Hackerrank/Sets.py
+++ Hackerrank/Sets.py
@@ -7,5 +7,9 @@ B = set([int(i) for i in input().split(' ')])
 
 #C = set([int(i) for i in input().split(' ')]) list comprehension
 #D = set([int(i) for i in input().split(' ')])
-
 print(sum((1 for i in S if i in A)) - sum((1 for i in S if i in B)))
+
+
+
+#Add and count the number of elements
+print(len(set([input() for i in range(int(input()))])))
