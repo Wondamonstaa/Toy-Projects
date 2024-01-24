ef58c1a8c5a47709bd500d4866a0de7aa23cf42e
| | /≥ \/ |_ <113752537+Wondamonstaa@users.noreply.github.com>
2024-01-23 20:33:28 -0600

    Update Itertools.py

--- Hackerrank/Itertools.py
+++ Hackerrank/Itertools.py
@@ -26,6 +26,13 @@ from itertools import combinations
 s, k = input().split()
 letters = sorted(s)
 
+#Combinations with replacement
+from itertools import combinations_with_replacement as cwr
+
+S, k = input().split()
+
+print("\n".join(["".join(e) for e in cwr(sorted(S), int(k))]))
+
 for i in range(1, int(k) + 1):
     for combination in combinations(letters, i):
         print("".join(combination))
