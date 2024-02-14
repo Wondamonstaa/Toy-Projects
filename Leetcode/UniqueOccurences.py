172ea85943b3b8ee39542d93a8145cc72a24be70
| | /≥ \/ |_ <113752537+Wondamonstaa@users.noreply.github.com>
2024-02-13 21:16:45 -0600

    Create UniqueOccurences.py

---
+++ Leetcode/UniqueOccurences.py
@@ -0,0 +1,10 @@
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+
+        freq = {}
+
+        for x in arr:
+            freq[x] = freq.get(x,0) + 1
+
+        return len(freq) == len(set(freq.values()))
+        #return max(Counter(Counter(arr).values()).values())<2
