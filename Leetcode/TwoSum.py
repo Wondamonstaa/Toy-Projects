382d1c4e71ab6e7616206a0394f8450a9efadb8c
| | /≥ \/ |_ <113752537+Wondamonstaa@users.noreply.github.com>
2024-03-07 23:49:26 -0600

    Update TwoSum.py

--- Leetcode/TwoSum.py
+++ Leetcode/TwoSum.py
@@ -1,14 +1,19 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
 
-        # Hash set
+        # Initialize the hash table
         h = {}
+
+        # Enumerate
 
         for i, num in enumerate(nums):
 
+            # Complement
             complement = target - num
-
+            
+            # Check if complement in a hash table already
             if complement in h:
                 return [h[complement], i]
+
 
             h[num] = i
