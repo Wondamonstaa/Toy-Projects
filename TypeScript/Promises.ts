9f3d3940e7b1e4cdbd1480879ec64f712654b69b
| | /≥ \/ |_ <113752537+Wondamonstaa@users.noreply.github.com>
2024-03-24 13:30:44 -0500

    Update Promises.ts

--- TypeScript/Promises.ts
+++ TypeScript/Promises.ts
@@ -9,10 +9,34 @@ interface LukeSkywalker {
   gender: string;
 }
 
+// First Option
 export const fetchLukeSkywalker = async (): Promise<LukeSkywalker> => {
   const data = await fetch("https://swapi.dev/api/people/1").then((res) => {
     return res.json();
   });
 
   return data;
+};
+
+// Second Option
+export const fetchLukeSkywalker = async () => {
+  const data: LukeSkywalker = await fetch("https://swapi.dev/api/people/1").then((res) => {
+    return res.json();
+  });
+
+  return data;
+};
+
+// Will work now
+// data.name ... data.mass etc.
+
+// Third Option
+export const fetchLukeSkywalker = async () => {
+  const data = await fetch("https://swapi.dev/api/people/1").then((res) => {
+    return res.json();
+  });
+
+  // const matt = {} as LukeSkywalker; => this is the stronger way to tell TS about the type of the object
+  
+  return data as LukeSkywalker;
 };
