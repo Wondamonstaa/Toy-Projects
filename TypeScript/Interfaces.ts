f6671c963cef3c3c87f29bbbe956b7b6cd430fc3
| | /≥ \/ |_ <113752537+Wondamonstaa@users.noreply.github.com>
2024-03-24 14:36:01 -0500

    Create Interfaces.ts

---
+++ TypeScript/Interfaces.ts
@@ -0,0 +1,25 @@
+import { Equal, Expect } from "./helpers/type-utils";
+
+interface Base {
+  id: string;
+};
+
+interface User extends Base{
+  firstName: string;
+  lastName: string;
+}
+
+interface Post extends Base{
+  title: string;
+  body: string;
+}
+
+interface Comment extends Base{
+  comment: string;
+}
+
+type tests = [
+  Expect<Equal<User, { id: string; firstName: string; lastName: string }>>,
+  Expect<Equal<Post, { id: string; title: string; body: string }>>,
+  Expect<Equal<Comment, { id: string; comment: string }>>,
+];
