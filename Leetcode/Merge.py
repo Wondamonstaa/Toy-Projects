9fe47b2860e353f4eaf814923f50afb8e28e068e
| | /≥ \/ |_ <113752537+Wondamonstaa@users.noreply.github.com>
2024-06-08 21:07:53 -0500

    Update Merge.py

--- Leetcode/Merge.py
+++ Leetcode/Merge.py
@@ -10,3 +10,20 @@ class Solution:
 
         nums1.extend(nums2)
         nums1.sort()
+
+#OR
+class Solution2:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+
+        nums3 = [x for x in nums1[:m]]
+        
+        nums3.extend(nums2)
+        
+        nums3.sort()
+        
+        for i in range(len(nums3)):
+            nums1[i] = nums3[i]
+        
