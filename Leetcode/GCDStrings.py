db0fec963d0c046391f82b2db44eebf44a63d356
| | /≥ \/ |_ <113752537+Wondamonstaa@users.noreply.github.com>
2024-09-01 14:45:35 -0500

    Update GCDStrings.py

--- Leetcode/GCDStrings.py
+++ Leetcode/GCDStrings.py
@@ -23,3 +23,19 @@ class Solution:
         
         # Return the substring of length gcd_length from str1
         return str1[:gcd_length]
+
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        
+        if str1 + str2 != str2 + str1:
+            return ""
+        
+        def gcd(a: int, b: int) -> int:
+
+            while b:
+                a, b = b, a % b
+            return a
+ 
+        prefix = gcd(len(str1), len(str2))
+
+        return str1[:prefix]
