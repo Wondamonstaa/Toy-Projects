8fafb68ec734451892394fdf9010a3ad30865d71
| | /≥ \/ |_ <113752537+Wondamonstaa@users.noreply.github.com>
2024-09-03 19:40:47 -0500

    Update isSubsequence.py

--- Leetcode/isSubsequence.py
+++ Leetcode/isSubsequence.py
@@ -8,3 +8,31 @@ class Solution:
             ti += 1  # Always move to the next character in t
 
         return si == len(s)  # If si == len(s), s is a subsequence of t
+
+
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        
+        i = j = 0
+        ss, tt = len(s), len(t)
+
+        if not s and t:
+            return True
+        
+        if s and not t:
+            return False
+        
+        if not s and not t:
+            return True
+
+        while i < ss and j < tt:
+            if s[i] == t[j]:
+                i += 1
+                j += 1
+            else:
+                j += 1
+            
+            if i == ss:
+                return True
+        
+        return False
