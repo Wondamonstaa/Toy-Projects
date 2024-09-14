12d3fe226dae251bdf9ca536f17fe020866a9e66
| | /≥ \/ |_ <113752537+Wondamonstaa@users.noreply.github.com>
2024-09-14 11:57:48 -0500

    Update MaxNumVowels.py

--- Leetcode/MaxNumVowels.py
+++ Leetcode/MaxNumVowels.py
@@ -8,31 +8,17 @@ class Solution:
         n = len(s)
         count = 0
         maxi = 0
-        i = 0
 
-        '''while i < n:
-        
-            window = s[i:k]
-            count = sum(1 for char in window if char in vowels)
-
-            if count <= k and count > maxi:
-                maxi = count
-                  
-            i += 1
-            k += 1'''
-        
-        # The first window
         for i in range(k):
             if s[i] in vowels:
                 count += 1
-        maxi = count
+            maxi = count
 
-        # Slide from left to right
         for i in range(k, n):
-            if s[i] in vowels:  # Add the new character at the right end of the window
+            if s[i] in vowels:
                 count += 1
-            if s[i - k] in vowels:  # Remove the character that's left behind as the window slides
+            if s[i-k] in vowels:
                 count -= 1
-            maxi = max(maxi, count)  # Update the maximum count of vowels
+            maxi = max(maxi, count)
 
         return maxi
