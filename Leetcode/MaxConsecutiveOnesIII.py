56ed5dedf8215aee4e539335d5598e788cf85702
| | /≥ \/ |_ <113752537+Wondamonstaa@users.noreply.github.com>
2024-09-15 23:54:32 -0500

    Update MaxConsecutiveOnesIII.py

--- Leetcode/MaxConsecutiveOnesIII.py
+++ Leetcode/MaxConsecutiveOnesIII.py
@@ -4,11 +4,13 @@ class Solution:
         start = end = 0
 
         for end in range(len(nums)):
+            print(f"Current nums[end]: {nums[end]}")
             if nums[end] == 0:
+                print(f"Start: {start}, end: {end}")
                 k -= 1
             if k < 0:
                 if nums[start] == 0:
                     k += 1
                 start += 1
-                
+
         return end - start + 1
