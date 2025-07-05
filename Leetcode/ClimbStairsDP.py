8ca4ca2fd859e8696d6345c61825b8c67fc023d3
| | /≥ \/ |_ <113752537+Wondamonstaa@users.noreply.github.com>
2025-07-05 08:09:19 -0500

    <>

--- Leetcode/ClimbStairsDP.py
+++ Leetcode/ClimbStairsDP.py
@@ -10,3 +10,22 @@ class Solution:
             return memo[n]
         
         return fib(n)
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        """
+        n=4:
+        1*4=4
+        1*2+2*2=4
+        2*2=4
+        2*2+1*2=4
+        """
+
+        def fib(i):
+            if i == n:
+                return 1
+            if i > n:
+                return 0
+            return fib(i+1) + fib(i+2)
+        
+        return fib(0)
