f2c33403247c174526bc1aa76674c117b3b4cdd2
| | /≥ \/ |_ <113752537+Wondamonstaa@users.noreply.github.com>
2025-07-08 13:37:16 -0500

    <>

--- Leetcode/GCD.py
+++ Leetcode/GCD.py
@@ -15,3 +15,22 @@ class Solution:
         #gcd = self.gcd(b, a)
 
         return gcd(a,b)
+
+
+import math
+
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+
+        if not str1:
+            return str2
+
+        if not str2:
+            return str1
+
+        if str1 + str2 != str2 + str1:
+            return ""
+
+        gcd = math.gcd(len(str1), len(str2))
+
+        return str1[:gcd] if len(str1) > len(str2) else str2[:gcd]
