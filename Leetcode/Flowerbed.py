501b7fe09eaa42f20845f2376d5f1c9cb0444d7b
| | /≥ \/ |_ <113752537+Wondamonstaa@users.noreply.github.com>
2025-07-08 14:26:26 -0500

    <>

--- Leetcode/Flowerbed.py
+++ Leetcode/Flowerbed.py
@@ -11,3 +11,22 @@ class Solution:
                 n -= 1
         
         return n <= 0
+
+
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        if not flowerbed:
+            return False
+
+        i = 0
+
+        while i < len(flowerbed):
+            if flowerbed[i] == 0 and (i == 0 or flowerbed[i - 1] == 0) and (i == len(flowerbed) - 1 or flowerbed[i + 1] == 0):
+                flowerbed[i] = 1
+                n -= 1
+                i += 2
+            else:
+                i += 1
+                n = n
+            
+        return n <= 0
