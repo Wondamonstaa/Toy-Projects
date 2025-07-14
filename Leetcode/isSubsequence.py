67b9d5cf5b5cd152887e52f1dedb36341352f6dc
| | /≥ \/ |_ <113752537+Wondamonstaa@users.noreply.github.com>
2025-07-14 12:55:56 -0500

    <>

--- Leetcode/isSubsequence.py
+++ Leetcode/isSubsequence.py
@@ -36,3 +36,16 @@ class Solution:
                 return True
         
         return False
+
+
+# New
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+
+        s_ptr = 0
+
+        for char in t:
+            if s_ptr < len(s) and s[s_ptr] == char:
+                s_ptr += 1
+
+        return s_ptr == len(s)
