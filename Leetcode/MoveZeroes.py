9d6c3c3282bb8d42255348c8bc8a9013a6868ca1
| | /≥ \/ |_ <113752537+Wondamonstaa@users.noreply.github.com>
2025-07-14 12:44:19 -0500

    <>

--- Leetcode/MoveZeroes.py
+++ Leetcode/MoveZeroes.py
@@ -27,3 +27,24 @@ class Solution:
                 
         for i in range(z, len(nums)):
             nums[i] = 0
+
+
+# Second
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+
+        start = 0 
+        c = 0
+        
+        for num in nums:
+            if num != 0:
+                nums[start] = num
+                start += 1
+
+        while start < len(nums):
+            nums[start] = 0
+            start += 1
