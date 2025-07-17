b498659f3efead3164f9ce35cf319cf0e588f976
| | /≥ \/ |_ <113752537+Wondamonstaa@users.noreply.github.com>
2025-07-16 23:15:54 -0500

    Logic added

--- Leetcode/longestSubarray.py
+++ Leetcode/longestSubarray.py
@@ -5,6 +5,22 @@ class Solution:
         max_len = 0
 
         for right in range(len(nums)):
+            
+            ''' 
+            [1,1,0,1]
+            nums[0] == 1: zero_count = 0
+            max_len = max(0, 0-0) = max(0,0) = 0
+
+            nums[1] == 1: zero_count = 0
+            max_len = max(0, 1-0) = max(0,1) = 1
+
+            nums[2] == 0: zero_count += 1
+            max_len = max(1, 2 - 0) = max(1,2) = 2
+
+            nums[3] == 1: zero_count = 1
+            max_len = max(2, 3-0) = max(2,3) = 3
+            '''
+
             if nums[right] == 0:
                 zero_count += 1
 
