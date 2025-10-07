d7d1312f421788e2004bb86145a92414b86367a2
| | /≥ \/ |_ <113752537+Wondamonstaa@users.noreply.github.com>
2025-10-07 05:05:52 -0500

    Create lenOfLastWord.py

---
+++ Leetcode/lenOfLastWord.py
@@ -0,0 +1,10 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+
+        if not s:
+            return 0
+        
+        s = s.strip().split()
+
+        return len(s[-1])
+        
