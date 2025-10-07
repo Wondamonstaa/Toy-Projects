cf5b1bf88738f310c4219ead3d62e96f849a64e1
| | /≥ \/ |_ <113752537+Wondamonstaa@users.noreply.github.com>
2025-10-07 05:00:53 -0500

    <>

---
+++ Leetcode/longestCommonPrefix.py
@@ -0,0 +1,16 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        
+        if not strs:
+            return ""
+
+        strs = sorted(strs)
+        prefix = ""
+
+        for i in range(min(len(strs[0]), len(strs[-1]))):
+            if strs[0][i] == strs[-1][i]:
+                prefix += strs[0][i]
+            else:
+                break
+
+        return prefix
